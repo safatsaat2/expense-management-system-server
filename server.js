c2923cb189894f8a402f1fc1df5f2a40ba0cbf38
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-04 02:34:59 -0700

    Set the server Installed: express, cors, morgan, dotenv, colors

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require("cors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const colors = require("colors")
+// config dot env
+
+dotenv.config();
+
+
+// rest object
+const app = express()
+
+// middleware
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+// routes
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello From Server</h1>')
+})
+
+// port
+
+const PORT = 8080 || process.env.PORT
+
+// listen server
+app.listen(PORT, () =>{
+    console.log(`Server running on ${PORT}`);
+});
