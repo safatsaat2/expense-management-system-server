14dda11f39e6a9d6b126c8a6a3436bd53012a259
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-05 00:44:52 -0700

    updated controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
-const userModel = require('../models/user')
+const userModel = require("../models/user");
 
 // Login Callback
-const loginController = async () => {};
+const loginController = async () => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email, password });
+
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      err,
+    });
+  }
+};
 
 // Register Callback
 
