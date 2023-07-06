c35a649237e885f25ecee36ab33dcbea136b5aff
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-06 03:19:46 -0700

    created: header, footer, layout

---
+++ client/src/Components/Layouts/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light'>
+            <h6 className="text-center">All rights reserved &copy; Sakibur Rahman Safat</h6>
+        </div>
+    );
+};
+
+export default Footer;
