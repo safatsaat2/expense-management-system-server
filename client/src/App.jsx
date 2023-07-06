788b6c110d6b9c2803f2e927a21bdd299638b9c4
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-06 01:07:00 -0700

    Added: client side, dependencies installed: axios, react-router-dom, redux react-redux

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container-fluid'>
+          <h1 className='text-center'>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
