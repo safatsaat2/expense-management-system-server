25bbe89668b07c3afb4d08f736e572c90b6d3b96
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-07 07:48:12 -0700

    submithandle is done for registration

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,18 +1,33 @@
-import {Form, Input} from 'antd'
+import {Form, Input, message} from 'antd'
 import { Link, useNavigate } from 'react-router-dom';
+import {axios} from 'axios'
+import { useState } from 'react';
+import Spinner from '../Components/Spinner';
 
 const Register = () => {
 const navigate = useNavigate()
+const [loading, setLoading] = useState(false)
 
 // Form Submit
-    const submitHandlers = (values) =>{
-        console.log(values)
+    const submitHandlers = async (values) =>{
+        try {
+          setLoading(true)
+          await axios.post('/users/register', values)
+          message.success('Registration successful')
+          setLoading(false)
+          navigate('/')
+        } catch (error) {
+          setLoading(true)
+          message.error("Invalid user or password")
+          setLoading(false)
+        }
     }
 
 
   return (
     <>
       <div className="register-page">
+        {loading && <Spinner/>}
         <Form layout='vertical' onFinish={submitHandlers}>
             <h1>Register Form</h1>
             <Form.Item label="Name:" name='name'>
